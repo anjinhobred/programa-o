f14214d8fefccbc094293e5e985317797e1f952e
anjinhobred <128755290+anjinhobred@users.noreply.github.com>
2023-04-13 20:50:43 -0300

    cliente e endereco

---
+++ banco/Cliente.py
@@ -0,0 +1,7 @@
+class Cliente:
+     def __init__(self,nome, sobrenome, telefone, endereço, nascimento):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.telefone= telefone
+        self.endereço = endereço
+        self.nascimento = nascimento
---
+++ banco/endereco.py
@@ -0,0 +1,9 @@
+class Endereco:
+    def __init__(self, cep, rua, numero, bairro,cidade,estado,complemento):
+        self. cep = cep
+        self.rua = rua 
+        self.numero= numero
+        self.bairro = bairro
+        self.complemento= complemento
+        self.cidade= cidade
+        self.estado= estado
